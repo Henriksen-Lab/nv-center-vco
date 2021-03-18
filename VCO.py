556565238be23cf1bec8dfc1fff9f31b13d31ff6
Jeff Ahlers <49463671+jmahlers@users.noreply.github.com>
2021-03-18 13:36:01 -0500

    Init

---
+++ VCO.py
@@ -0,0 +1,14 @@
+# This is a module that contains a callable
+import numpy as np
+from os import path
+import numpy as np
+
+resources_dir = path.join(path.dirname(__file__), 'resources')
+splinePath = resources_dir+"/VCO_interpolated_spline_object.npy"
+interpolatedCurve = np.load(
+    splinePath, allow_pickle=True)[()]
+
+
+def voltageToFrequency(voltage):
+    '''Returns the frequency outputted for a given voltage based on a linear univariate spline interpolation with S=0.0001'''
+    return interpolatedCurve(voltage)
